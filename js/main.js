b7f125f8539e4dea436548925b283cc66c37f81e
José Vitor <jose-vitor-7@hotmail.com>
2020-03-23 06:31:27 -0300

    Criação de animação para os textos

--- js/main.js
+++ js/main.js
@@ -5,33 +5,43 @@ const liEl3 = document.querySelector('#toS3');
 const sectionEl1 = document.querySelector('#s1');
 const sectionEl2 = document.querySelector('#s2');
 const sectionEl3 = document.querySelector('#s3');
+// console.log(document.querySelector('.textContainer').classList);
+// document.querySelector('.textContainer').classList.remove('textContainerActive');
+// console.log(document.querySelector('.textContainer').classList);
 
 sectionEl1.scrollIntoView({
     behavior: 'smooth',
     block: 'start'
 });
 
+
 liEl1.onclick = () => {
-    sectionEl1.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+    setTimeout(() => {
+        sectionEl1.scrollIntoView({
+            // behavior: 'smooth',
+            block: 'start'
+        });    
+    }, 500);
     setActualSection(1);
 }
 
 liEl2.onclick = () => {
-    sectionEl2.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+    setTimeout(() => {
+        sectionEl2.scrollIntoView({
+            // behavior: 'smooth',
+            block: 'start'
+        });    
+    }, 500);
     setActualSection(2);
 }
 
 liEl3.onclick = () => {
-    sectionEl3.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+    setTimeout(() => {
+        sectionEl3.scrollIntoView({
+            // behavior: 'smooth',
+            block: 'start'
+        });    
+    }, 500);
     setActualSection(3);
 }
 
@@ -54,20 +64,26 @@ window.addEventListener('wheel', (event) => {
     }
 
     if(actualSection === 1){
-        sectionEl1.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        setTimeout(() => {
+            sectionEl1.scrollIntoView({
+                // behavior: 'smooth',
+                block: 'start'
+            });    
+        }, 500);
     }else if(actualSection === 2){
-        sectionEl2.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        setTimeout(() => {
+            sectionEl2.scrollIntoView({
+                // behavior: 'smooth',
+                block: 'start'
+            });    
+        }, 500);
     }else if(actualSection === 3){
-        sectionEl3.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        setTimeout(() => {
+            sectionEl3.scrollIntoView({
+                // behavior: 'smooth',
+                block: 'start'
+            });    
+        }, 500);
     }
 });
 
@@ -75,12 +91,27 @@ function setActualSection(val){
     if (val === 1){
         actualSection = 1;
         document.querySelector('#indicatorBall').style.top = 0;
+        document.querySelector('#s2 .textContainer').classList.remove('textContainerActive');
+        document.querySelector('#s3 .textContainer').classList.remove('textContainerActive');
+        setTimeout(() => {
+            document.querySelector('#s1 .textContainer').classList.add('textContainerActive');    
+        }, 150);
     }else if (val === 2){
         actualSection = 2;
         document.querySelector('#indicatorBall').style.top = '50%';
+        document.querySelector('#s1 .textContainer').classList.remove('textContainerActive');
+        document.querySelector('#s3 .textContainer').classList.remove('textContainerActive');
+        setTimeout(() => {
+            document.querySelector('#s2 .textContainer').classList.add('textContainerActive');    
+        }, 150);
     }else if (val === 3){
         actualSection = 3;
         document.querySelector('#indicatorBall').style.top = '100%';
+        document.querySelector('#s1 .textContainer').classList.remove('textContainerActive');
+        document.querySelector('#s2 .textContainer').classList.remove('textContainerActive');
+        setTimeout(() => {
+            document.querySelector('#s3 .textContainer').classList.add('textContainerActive');    
+        }, 150);
     }else{
         console.warn('Valor de parametro inválido. O valor deve ser de 1 a 3.')
     }
