4b7faa3c0834108afbd78ca2f3e611d1649e7dc4
José Vitor <jose-vitor-7@hotmail.com>
2020-03-23 03:16:04 -0300

    Scroll com roda do mouse feito

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-var liEl1 = document.querySelector('#toS1');
-var liEl2 = document.querySelector('#toS2');
-var liEl3 = document.querySelector('#toS3');
-
-var sectionEl1 = document.querySelector('#s1');
-var sectionEl2 = document.querySelector('#s2');
-var sectionEl3 = document.querySelector('#s3');
+var actualSection = 1;
+const liEl1 = document.querySelector('#toS1');
+const liEl2 = document.querySelector('#toS2');
+const liEl3 = document.querySelector('#toS3');
+const sectionEl1 = document.querySelector('#s1');
+const sectionEl2 = document.querySelector('#s2');
+const sectionEl3 = document.querySelector('#s3');
 
 liEl1.onclick = () => {
     sectionEl1.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
     });
+    actualSection = 1;
 }
 
 liEl2.onclick = () => {
@@ -18,6 +19,7 @@ liEl2.onclick = () => {
         behavior: 'smooth',
         block: 'start'
     });
+    actualSection = 2;
 }
 
 liEl3.onclick = () => {
@@ -25,4 +27,41 @@ liEl3.onclick = () => {
         behavior: 'smooth',
         block: 'start'
     });
+    actualSection = 3;
 }
+
+
+window.addEventListener('wheel', (event) => {
+    if(event.deltaY < 0){
+        if(actualSection === 3){
+            actualSection = 2;
+        }else if(actualSection === 2){
+            actualSection = 1;
+        }
+        console.log(actualSection);
+    }else if(event.deltaY > 0){
+        if(actualSection === 1){
+            actualSection = 2;
+        }else if(actualSection === 2){
+            actualSection = 3;
+        }
+        console.log(actualSection);
+    }
+
+    if(actualSection === 1){
+        sectionEl1.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }else if(actualSection === 2){
+        sectionEl2.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }else if(actualSection === 3){
+        sectionEl3.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+});
